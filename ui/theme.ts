d7bb8c65887326fe3c1e86fcf62c401942fb0062
mareadelmar <mar.ortega.v@gmail.com>
2022-02-09 11:44:50 -0300

    Fix bug

--- ui/theme.ts
+++ ui/theme.ts
@@ -1,4 +1,4 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import createMuiTheme from '@material-ui/core/styles/createTheme'
 import { grey, common, lightGreen } from '@material-ui/core/colors'
 
 const muiTheme = createMuiTheme()
