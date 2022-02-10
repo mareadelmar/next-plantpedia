eed2f66c343e9b1cdf78548250d627dadf648217
mareadelmar <mar.ortega.v@gmail.com>
2022-02-10 12:38:55 -0300

    Fetch plants list with server side with static site generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
-import { useEffect, useState } from 'react'
 import { Layout } from '@components/Layout'
 import { getPlantList } from '@api'
 import { PlantCollection } from '@components/PlantCollection'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-export default function Home() {
-  const [resData, setResData] = useState<Plant[]>([])
 
-  useEffect(() => {
-    getPlantList({limit: 10}).then(setResData)
-  }, [])
+type HomeProps = { plants: Plant[] }
+
+export const getStaticProps:GetStaticProps<HomeProps> = async () => {
+
+  const plants = await getPlantList({limit: 10});
+
+  return {
+    props: {
+      plants
+    }
+  }
+}
+
+export default function Home({ plants }:InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <Layout>
-      <PlantCollection plants={resData}/>
+      <PlantCollection plants={plants} variant="square" className=""/>
     </Layout>
   )
 }
